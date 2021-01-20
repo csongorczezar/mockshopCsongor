84d8224a6b8d0b19a0f33abef1210328dcd7d46b
Csongor Czezar <csongor.czezar@gmail.com>
2021-01-19 19:56:06 -0800

    Applied style

--- src/components/CategoriesList.react.js
+++ src/components/CategoriesList.react.js
@@ -1,19 +1,57 @@
 import React from 'react';
-import { Spinner } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, CardActionArea, CardContent, CardMedia, CircularProgress, Grid, makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    card: {
+        minWidth:250,
+        maxWidth:250,
+        maxHeight:345,
+        margin:20
+    },
+    media: {
+        height:270
+    },
+    title: {
+        textAlign: 'center',
+        margin:50 
+    },
+    text: {
+        textTransform: "capitalize"
+    }
+})
+
 
 function CategoriesList(props) {
+    const classes = useStyles()
     const products = props.data
     console.log(products)
     return (
         <div>
-            <h1>Categories List Here</h1>
+            <h1 className={classes.title}>Welcome to Czezar's Mart</h1>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+            >
             {products?Object.keys(products).map(item=>(
-            <div>
-                <p>{item}</p>
-                <img src={products[item][0].image} alt={products[item][0].title}/>
-            </div>
-            )):<Spinner animation="border" variant="dark"/>}
+                <Card className={classes.card}>
+                    <CardActionArea>
+                        {/* <p>{item}</p> */}
+                        {/* <img src={products[item][0].image} alt={products[item][0].title}/> */}
+                        <CardMedia
+                            className= {classes.media}
+                            image = {products[item][0].image}
+                            title = {products[item][0].title}
+                        />
+                        <CardContent>
+                            <Typography variant="h5" component="h2" className={classes.text}>
+                                {item} 
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            )):<CircularProgress disableShrink />}
+            </Grid>
         </div>
     )
 }
