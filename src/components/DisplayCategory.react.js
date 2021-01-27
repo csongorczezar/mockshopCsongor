78680f792a746e07777a21eefc693c018439fc16
Csongor Czezar <csongor.czezar@gmail.com>
2021-01-27 14:31:19 -0800

    Created the design for the displayed goods

--- src/components/DisplayCategory.react.js
+++ src/components/DisplayCategory.react.js
@@ -1,17 +1,78 @@
+import { Box, Button, Card, CardActionArea, CardContent, CardMedia, Grid, makeStyles, Typography } from "@material-ui/core";
+import { ArrowBack } from "@material-ui/icons";
 import React from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
+
+const useStyles = makeStyles({
+    card: {
+        minWidth:250,
+        maxWidth:250,
+        maxHeight:500,
+        margin:20
+    },
+    media: {
+        height:270
+    },
+    title: {
+        textTransform: "capitalize",
+        textAlign: 'center',
+        margin:50 
+    },
+    text: {
+        textTransform: "capitalize",
+        height:30,
+        overflow:"hidden"
+    },
+    price: {
+        marginTop: 15,
+        fontWeight: "bold"
+    },
+    button: {
+        textAlign: "center",
+        marginTop: 50
+    }
+})
 
 
 function DisplayCategory() {
     const {category} = useParams()
     const productCatalog = useSelector(state=>state.products.productsGrouped)
+    const classes = useStyles()
+    const history = useHistory()
+    const goBack = () => history.push("/")
  
     return (
         <div>
-            {productCatalog[category].map((item,index)=>(
-                <h1 key={item.id}>{item.title}</h1>
-            ))}
+            <h1 className={`${classes.title}`} >{category}</h1>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+            >
+                {productCatalog[category].map((item,index)=>(
+                    <Card key={item.id} className={classes.card} >
+                        <CardActionArea>
+                            <CardMedia
+                                className= {classes.media}
+                                image = {item.image}
+                                title = {item.title}
+                            />
+                            <CardContent>
+                                <Typography variant="h5" component="h2" className={classes.text}>
+                                    {item.title}
+                                </Typography>
+                                <Typography variant="h5" component="h3" className={classes.price}>
+                                    Price: $ {item.price}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                ))}
+            </Grid>
+            <Box className={classes.button}>
+                <Button variant="contained" color="primary" startIcon={<ArrowBack/>} onClick={goBack}>Back</Button>
+            </Box>  
         </div>
         
     )
